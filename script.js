226a1a648a52e14ce230c4ca1e259fbe18231193
Redcoder807 <gearonhaynes@gmail.com>
2022-02-15 14:06:32 -0600

    Normal done

---
+++ script.js
@@ -0,0 +1,21 @@
+let warmHugs = ("Hi, I'm Olaf and I like warm hugs.");
+console.log(warmHugs.toUpperCase());
+warnHugs = console.log(warmHugs.replace("like", "love"));
+
+beenImpaled = ("Oh, look at that. I've been impaled.");
+console.log(beenImpaled.slice(18));
+
+dotDotDot = ("...");
+skullBones = (`I don't have a skull${dotDotDot}or bones.`)
+console.log(skullBones);
+
+console.log(Math.PI);
+
+randomNumber = Math.random();
+randomNumber *= 3
+randomNumber = Math.floor(randomNumber);
+randomNumber++;
+console.log(randomNumber);
+
+// bonus
+console.log(" Let It Go!".toUpperCase().trim().repeat(2));
